4db66ad8a804812b860a2f4a9be6091828ab0f24
javier <javieriranzo3@gmail.com>
2023-02-17 21:44:55 +0100

    automatico cypress-gha-example 17/02/2023

---
+++ cypress/e2e/1-getting-started/BASE_DATOS.cy.js
@@ -0,0 +1,30 @@
+/// <reference types="cypress" />
+// vamos a usar estas funciones en el test
+import { nombreFichero, escribeLog, escribeLogBefore, escribeLogAfter, f_url } from 'cypress-utilidades'; //  from "../support/utils";
+// cargo datos generales a PADDOC desde este archivo
+// const datosPaddoc = require('../fixtures/datosPaddoc.json');
+
+describe('BASE_DATOS', () =>
+{
+    // cargo variables para los tests   
+    var nombreFicheroLog = nombreFichero('BASE-DATOS');
+
+    // antes de empezar los test 
+    before(() =>
+    {
+        escribeLogBefore(nombreFicheroLog)
+    })
+
+  it('Comprobando la base de datos', () => {
+    escribeLog(nombreFicheroLog, 'Comprobando la base de datos')
+    cy.visit(f_url() + 'eppla_adm/test.jsp')
+    cy.contains('[ORACLE DB] - [OK]')
+    escribeLog(nombreFicheroLog, 'OK. Comprobada la base de datos')
+  })
+
+  // al finalizar los tests 
+  after(() =>
+  {
+      escribeLogAfter(nombreFicheroLog)
+  })
+})
